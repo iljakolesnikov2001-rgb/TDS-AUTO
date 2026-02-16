0299166390e16ffe8a229286cb48386762e602ed
iljakolesnikov2001-rgb <ilja.kolesnikov2001@gmail.com>
2026-02-16 11:01:26 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,27 +1,49 @@
--- Library.lua
+-- Library.lua - Все функции
 local Library = {}
 
-function Library:GetGameState()
-    local gui = game.Players.LocalPlayer.PlayerGui
-    if gui:FindFirstChild("ReactLobbyHud") then return "LOBBY" end
-    if gui:FindFirstChild("ReactUniversalHotbar") then return "GAME" end
-    return "UNKNOWN"
+-- Recorder (локальный)
+local recording = false
+local log = {}
+
+function Library:StartRecorder()
+    recording = not recording
+    print("Recorder: " .. (recording and "ВКЛ" or "ВЫКЛ"))
+    if not recording then
+        if #log > 0 then
+            writefile("Strat.txt", table.concat(log, "\n"))
+            print("Сохранено в Strat.txt (" .. #log .. " строк)")
+            log = {}
+        end
+    end
 end
 
-function Library:PlaceTower(tower, pos)
-    game:GetService("ReplicatedStorage").Events.PlaceTower:FireServer(tower, pos)
+function Library:Record(action)
+    if recording then
+        table.insert(log, action)
+    end
 end
 
-function Library:UpgradeTower(tower)
-    game:GetService("ReplicatedStorage").Events.UpgradeTower:FireServer(tower)
+-- Equip (локальный)
+local towers = {"Scout","Sniper","Paintballer","Demoman","Hunter","Soldier","Militant","Freezer","Assassin","Shotgunner","Pyromancer","Ace Pilot","Medic","Farm","Rocketeer","Trapper","Military Base","Crook Boss","Electroshocker","Commander","Warden","Cowboy","DJ Booth","Minigunner","Ranger","Pursuit","Gatling Gun","Turret","Mortar","Mercenary Base","Brawler","Necromancer","Accelerator","Engineer","Hacker","Gladiator","Commando","Frost Blaster","Archer","Swarmer","Toxic Gunner","Sledger","Executioner","Elf Camp","Jester","Cryomancer","Hallow Punk","Harvester","Snowballer","Elementalist","Firework Technician","Biologist","Warlock","Spotlight Tech","Mecha Base"}
+
+local selected = towers[1]
+
+function Library:EquipTower()
+    if game.PlaceId ~= 3260590327 then
+        print("Только в лобби TDS!")
+        return
+    end
+    game:GetService("ReplicatedStorage").RemoteEvent:FireServer("Towers", "Equip", selected)
+    print("Экипировано: " .. selected)
+    Library:Record("Equip " .. selected)
 end
 
-function Library:SellTower(tower)
-    game:GetService("ReplicatedStorage").Events.SellTower:FireServer(tower)
-end
-
-function Library:SkipWave()
-    game:GetService("ReplicatedStorage").Events.SkipWave:FireServer()
+-- Для смены башни (можно добавить кнопку в Main)
+function Library:NextTower()
+    local idx = table.find(towers, selected) or 1
+    idx = idx % #towers + 1
+    selected = towers[idx]
+    print("Выбрана башня: " .. selected)
 end
 
 return Library
